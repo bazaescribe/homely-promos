fbaef42e6bc9b17d7b968e1edca4253acf4235de
José Luis <jlrosasb@gmail.com>
2019-09-23 16:32:40 -0500

    Updated mail receiver

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
   $headers = 'From: baza@homely.mx' . "\r\n" .
   'Reply-To: hola@homely.mx' . "\r\n" .
   'X-Mailer: PHP/' . phpversion();
-  $to_Email       = "hola@homely.mx"; //Replace with recipient email address
+  $to_Email       = "baza@homely.mx"; //Replace with recipient email address
   $subject        = 'Contacto Proconvenciones'; //Subject line for emails
   $name = $_POST['name'];
   $email = $_POST['mail'];
